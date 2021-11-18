ba1ceaa4c954fd9513e8b13a97975456d8c6412a
marcosspano <e.marcosspano@gmail.com>
2021-11-18 17:36:35 -0300

    alterando

---
+++ src/main/java/dev/marcosspano/dockerspring/controller/Hello.java
@@ -0,0 +1,18 @@
+package dev.marcosspano.dockerspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Hello {
+
+    @RestController
+    public class Hello {
+        @GetMapping("/hello")
+        public String helloMessage() {
+            
+            return "Hello World";
+        }
+    }
+
+
+}
